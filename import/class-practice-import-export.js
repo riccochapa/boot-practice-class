51858f853fe4d6c194c675c6d1fc693cff7e40c8
Ricco <riccochapa@gmail.com>
2016-06-29 14:57:32 -0500

    Update

--- import/class-practice-import-export.js
+++ import/class-practice-import-export.js
@@ -9,7 +9,7 @@ var sumOfBalance = 0;
 
 var acc1 = new BankAccount('Jimmy Jonas');
 var acc2 = new SavingsAccount('Debbie Boone');
-var text = new BankAccount("Tommy San");
+var text = new BankAccount("Tommy Sans");
 
 acc1.withdrawal(222);
-text.withdrawal(333);
+text.withdrawal(333333);
